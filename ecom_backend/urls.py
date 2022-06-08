e3d804a6fd6d38df5023ff99e113a105e0fb9853
SyamCyberonics <syamjith@cyberonics.in>
2022-06-08 09:58:37 +0530

    swagger documentation enabled

--- ecom_backend/urls.py
+++ ecom_backend/urls.py
@@ -15,11 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
-    path('coupons/', include('coupons.urls'))
+    path('coupons/', include('coupons.urls')),
+    path('openapi', get_schema_view(
+        title="Ecommerce Backend",
+        description="API endpoints for all CRUD operations",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('', TemplateView.as_view(
+            template_name='swagger-ui.html',
+            extra_context={'schema_url': 'openapi-schema'}
+        ), name='swagger-ui'),
 ]
