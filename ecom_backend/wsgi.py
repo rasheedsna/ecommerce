3300624384e6f0c2a26e50dfd872c963d3e2b20a
SyamCyberonics <syamjith@cyberonics.in>
2022-06-15 17:05:39 +0530

    removed all socket io content

--- ecom_backend/wsgi.py
+++ ecom_backend/wsgi.py
@@ -7,17 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom_backend.settings')
+#
+# import socketio
+# from notifications.views import sio
+#
+# django_app = get_wsgi_application()
+# application = socketio.WSGIApp(sio, django_app)
+#
+# import eventlet.wsgi
+# eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom_project.settings')
 
-import socketio
-from notifications.views import sio
-
-django_app = get_wsgi_application()
-application = socketio.WSGIApp(sio, django_app)
-
-import eventlet.wsgi
-eventlet.wsgi.server(eventlet.listen(('', 8000)), application)
+application = get_wsgi_application()
